186e8f48600efda7749beaa3030f93e1fa93d7b6
HidellaS <sandeephidella@gmail.com>
2025-01-19 08:59:26 +0530

    completed data transformation and util

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 
 @dataclass
@@ -47,6 +49,9 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
         
-# if __name__=="__main__":
-#     obj=DataIngestion()
-#     obj.initiate_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
